783f3bf9143756d70a994fdf987258d4644145b7
vhollo <vhollo@gmail.com>
2020-03-06 18:48:14 +0100

    select qtype

--- src/App.svelte
+++ src/App.svelte
@@ -2,28 +2,42 @@
 	import Parosito from './qtypes/parosito.svelte';
 	import Felelet from './qtypes/felelet.svelte';
 	import { count } from './stores.js';
-/* 
-svelte-loadable
-https://www.npmjs.com/package/svelte-loadable
-*/
+	/*
+	svelte-loadable
+	https://www.npmjs.com/package/svelte-loadable
+	*/
+
+	const components = [
+		{ name: 'Párosító', qtype: Parosito, idx: 1 },
+		{ name: 'Feleletválasztó', qtype: Felelet, idx: 2 },
+	];
+
+	let selected = components[0];
+
 	let idx;
 </script>
 
 <header>
 	<h1>Quizzone experiment!</h1>
-	<h2>Kép-szöveg párosító</h2>
 	<p>Nem vagyunk "bezárva" egy képernyőbe, mint a projektoron! <br>Scrollozhatunk! Drag & drop! Select & pair!</p>
-  <span>1.: {$count[1]}</span><br>
+  <!-- <span>1.: {$count[1]}</span><br>
   <span>2.: {$count[2]}</span><br>
   <span>3.: {$count[3]}</span><br>
   <span>4.: {$count[4]}</span><br>
-  <span>5.: {$count[5]}</span><br>
+	<span>5.: {$count[5]}</span><br> -->
+	<select bind:value={selected}>
+		{#each components as component}
+			<option value={component}>{component.idx} {component.name}</option>
+		{/each}
+	</select>
+
 </header>
 
 <main id="questions">
-	<Felelet idx={1} />
+	<svelte:component this={selected.qtype} idx={selected.idx} />
+	<!-- <Felelet idx={1} />
 	<br>
-	<Parosito idx={2} />
+	<Parosito idx={2} /> -->
 </main>
 
 <footer>
